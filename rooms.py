b338de085258b71f735303a1aa0ad443adbe6e1f
Shug1876 <2549100C@student.gla.ac.uk>
2022-10-15 10:49:39 +0100

    Modified rooms files to give int number for previous room and number of mobs

--- rooms.py
+++ rooms.py
@@ -1,39 +1,24 @@
 import csv
+import random
 
 with open('rooms.csv', newline='') as csvfile:
     rooms = list(csv.reader(csvfile))
 rooms[0][0] = rooms[0][0].replace("\ufeff", '')
-#print(rooms[int(rooms[10][1])])
-print(rooms)
 
-#room2 = {"next":None, "back":room1, "items":None, "name":2}
-#start_room = {"next":None, "back":None, "items":None, "name":"start"}
-#room1 = {"next":None, "back":start_room, "items":None, "name":1}
-#room3 = {"next":None, "back":room2, "items":None, "name":3}
-#room4 = {"next":None, "back":room3, "items":None, "name":4}
-#room5 = {"next":None, "back":room4, "items":None, "name":5}
-#room6 = {"next":None, "back":room5, "items":None, "name":6}
-#room7 = {"next":None, "back":room6, "items":None, "name":7}
-#room8 = {"next":None, "back":room7, "items":None, "name":8}
-#room9 = {"next":None, "back":room8, "items":None, "name":9}
-#room10 = {"next":None, "back":room9, "items":None, "name":10}
-#bigger_room = {"next":None, "back":None, "items":None, "name":"big1"}
-#bigger_room2 = {"next":None, "back":None, "items":"key", "name":"big2"}
-#hidden_room = {"next":None, "back":None, "items":None, "name":"hidden"}
-#end_room = {"next":"END", "back":None, "items":None, "name":"final"}
-#rooms = [room1, room2, room3, room4, room5, room6, room7, room8, room9, room10, bigger_room, bigger_room2]
-
-#start_room["next"] = room1
-#room1["next"] = room2
-#room2["next"] = room3
-#room3["next"] = room4
-#room4["next"] = room5
-#room5["next"] = room6
-#room6["next"] = room7
-#room7["next"] = room8
-#room8["next"] = room9
-#room9["next"] = room10
+for room in rooms:
+    print(room)
+    mob_count = room[-1]
+    
+    if '/' in mob_count:
+        mob_number = mob_count.split("/")
+        for i in range(len(mob_number)):
+            mob_number[i] = int(mob_number[i])
+        mob_count = random.choice(mob_number)
+    else:
+        mob_count = int(mob_count)
+    room[-1] = mob_count
+    room[1] = int(room[1])
+    print(room)
+    print()
 
 current_room = rooms[0]
-
-#print(current_room)
