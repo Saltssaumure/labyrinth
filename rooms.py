7dc05a0458df0dc296e06b29ff329b9a548492ab
Shug1876 <2549100C@student.gla.ac.uk>
2022-10-15 12:48:47 +0100

    added hallway rooms and comments for indicating what each index is

--- rooms.py
+++ rooms.py
@@ -7,8 +7,12 @@ rooms[0][0] = "start_room"
 
 mobs = ["mob1", "mob2", "mob3", "mob4", "mob5", "mob6", "mob7", "mob8"] # mob names set to change
 
+# what each index is:
+# room name, back room, room ahead, [room to left, room to right] (0 for no rooms in hallway), Items, Number of mobs in room, list of mobs in room (randomly selected from list of mobs)
+
 for room in rooms:
     mob_count = room[-1]
+    corridor_rooms = room[3]
     
     if '/' in mob_count:
         mob_number = mob_count.split("/")
@@ -17,9 +21,18 @@ for room in rooms:
         mob_count = random.choice(mob_number)
     else:
         mob_count = int(mob_count)
+      
+    if '|' in corridor_rooms:
+        corridor_rooms = corridor_rooms.split("|")
+        for i in range(len(corridor_rooms)):
+            corridor_rooms[i] = int(corridor_rooms[i])
+    else:
+        corridor_rooms = int(corridor_rooms)
+        if corridor_rooms > 0:
+            corridor_rooms = [corridor_rooms]
+    room[3] = corridor_rooms
     room[-1] = mob_count
     room[1] = int(room[1])
-    room[-2] = int(room[-2])
     try:
         room[2] = int(room[2])
     except:
@@ -33,7 +46,6 @@ for room in rooms:
     print()
 
 #print(rooms)
-print()
-current_room = rooms[1]
+current_room = rooms[15]
 print(current_room)
 print(rooms[rooms[1][1]])
