7f44052369156b44239e6a838d0c689cedd5bd3f
Shug1876 <2549100C@student.gla.ac.uk>
2022-10-15 11:33:28 +0100

    Modified rooms files
    added extra row to end list to show a list of what mobs are in the room, corrected a before room index for the hidden room in the csv file

--- rooms.py
+++ rooms.py
@@ -5,6 +5,8 @@ with open('rooms.csv', newline='') as csvfile:
     rooms = list(csv.reader(csvfile))
 rooms[0][0] = rooms[0][0].replace("\ufeff", '')
 
+mobs = ["mob1", "mob2", "mob3", "mob4", "mob5", "mob6", "mob7", "mob8"] # mob names set to change
+
 for room in rooms:
     mob_count = room[-1]
     
@@ -18,7 +20,9 @@ for room in rooms:
     room[-1] = mob_count
     room[1] = int(room[1])
     room[-2] = int(room[-2])
+    if room[-1]>0:
+        room += [random.sample(mobs, room[-1])]
 
-print(rooms)
+#print(rooms)
 
 current_room = rooms[0]
